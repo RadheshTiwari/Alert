1429859b7e21fb4e7c50237e6b9f02fc372e945b
RadheshTiwari <radheshtiwaripddu@gmail.com>
2023-04-23 19:19:27 +0530

    Alert

---
+++ App.js
@@ -0,0 +1,161 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useState} from 'react';
+import type {Node} from 'react';
+import {
+  Alert,
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+// import {
+//   Colors,
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
+
+// /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+//  * LTI update could not be added via codemod */
+// const Section = ({children, title}): Node => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// };
+
+// const App: () => Node = () => {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+//   const backgroundStyle = {
+//     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+//   };
+
+//   return (
+//     <SafeAreaView style={backgroundStyle}>
+//       <StatusBar
+//         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+//         backgroundColor={backgroundStyle.backgroundColor}
+//       />
+//       <ScrollView
+//         contentInsetAdjustmentBehavior="automatic"
+//         style={backgroundStyle}>
+//         <Header />
+//         <View
+//           style={{
+//             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+//           }}>
+//           <Section title="Step One">
+//             Edit <Text style={styles.highlight}>App.js</Text> to change this
+//             screen and then come back to see your edits.
+//           </Section>
+//           <Section title="See Your Changes">
+//             <ReloadInstructions />
+//           </Section>
+//           <Section title="Debug">
+//             <DebugInstructions />
+//           </Section>
+//           <Section title="Learn More">
+//             Read the docs to discover what to do next:
+//           </Section>
+//           <LearnMoreLinks />
+//         </View>
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
+
+const App = () => {
+  const [x, setx] = useState();
+  const [y, sety] = useState();
+  //var setz;
+  const [z, setz] = useState();
+  return (
+    <View>
+      <TextInput
+        placeholder="value1"
+        style={{fontSize: 30, borderWidth: 2, borderColor: 'black'}}
+        onChangeText={input1 => setx(input1)}
+        value={x}></TextInput>
+      <TextInput
+        placeholder="value2"
+        style={{fontSize: 30, borderWidth: 2, borderColor: 'black'}}
+        onChangeText={input2 => sety(input2)}
+        value={y}></TextInput>
+      <Button
+        title="click"
+        onPress={() => {
+          let result = Number(x) + Number(y);
+
+          if (result % 2 == 0) {
+            Alert.alert('even');
+          } else {
+            Alert.alert('odd');
+          }
+          setz(result);
+        }}
+      />
+      <Button
+        title="RESET"
+        onPress={() => {
+          setz == 0;
+          setx(0);
+          sety(0);
+        }}
+      />
+    </View>
+  );
+};
+export default App;
